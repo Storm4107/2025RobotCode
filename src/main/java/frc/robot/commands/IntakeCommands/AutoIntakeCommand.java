32a9c113c1dd806625f7f203d5c76df3661d2d6f
Reichen <119686482+Breichen1@users.noreply.github.com>
2025-03-11 18:47:22 -0500

    Update AutoIntakeCommand.java

--- src/main/java/frc/robot/commands/IntakeCommands/AutoIntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/AutoIntakeCommand.java
@@ -5,12 +5,16 @@
 package frc.robot.commands.IntakeCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Intake;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AutoIntakeCommand extends Command {
   /** Creates a new AutoIntakeCommand. */
-  public AutoIntakeCommand() {
+  private Intake s_Intake;
+  public AutoIntakeCommand(Intake s_Intake) {
+    this.s_Intake = s_Intake;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(s_Intake);
   }
 
   // Called when the command is initially scheduled.
@@ -19,15 +23,19 @@ public class AutoIntakeCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    s_Intake.setVoltage(2);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    s_Intake.setVoltage(2);
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return s_Intake.coralSensor();
   }
 }
